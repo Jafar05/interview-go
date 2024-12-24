22b3f62a9dacf95f869969067c77e556cabc3273
jafar <jafar_magomedov0594@mail.ru>
2024-12-24 14:58:06 +0300

    update slice readme file

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	sl := make([]int, 0, 3)
+
+	sl = append(sl, 1, 2)
+	addSl(sl)
+
+	fmt.Println(sl)
+}
+
+func addSl(sl []int) {
+	sl = append(sl, 3)
 }

d5adaf9f1e21e4a9b6472bc3397037bd2e5476f8
jafar <jafar_magomedov0594@mail.ru>
2025-01-01 17:57:31 +0300

    update readme

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	a := [3]int{1, 2, 3}
-	b := [3]int{1, 2, 3}
-	c := [3]int{1, 2, 4}
+	a := "Hello"
+	fmt.Printf("a - %x, %q\n", &a, a)
 
-	fmt.Println(a == b) // true (все элементы равны)
-	fmt.Println(a == c) // false (различие в последнем элементе)
+	a = "world"
+	fmt.Printf("a - %x, %q\n", &a, a)
+
+	b := a[0:2]
+	fmt.Printf("b - %x, %q\n", &b, b)
 }

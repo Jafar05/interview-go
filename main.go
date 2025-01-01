848b756309dcaa0c60786868358e476214af2261
jafar <jafar_magomedov0594@mail.ru>
2025-01-01 15:19:29 +0300

    update readme

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
+	src := []int{1, 2, 3, 4}
+	dst := make([]int, 5) // Целевой срез длиной 5
 
-}
-
-// На вход подается канал значения из которого нужно перекладывать в оба канала
-// Например если в канал записывается 1, 3, 5, то в канал a должно записаться 1, 3, 5 и канал b  должно записаться 1, 3, 5
-
-func tee(in <-chan int) (chan any, chan any) {
-	a := make(chan any, 1)
-	b := make(chan any, 1)
-
-	go func() {
-		defer close(a)
-		defer close(b)
-		for val := range in {
-			a <- val
-			b <- val
-		}
-	}()
-
-	return a, b
+	copy(dst, src) // Копируем из src в dst
+	fmt.Println("Source:", src)
+	fmt.Println("Destination:", dst)
 }

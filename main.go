3fd5c6fa00c92f971eaeb7ac2c1e579a3f31cb0d
jafar <jafar_magomedov0594@mail.ru>
2025-01-06 16:42:28 +0300

    update

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "interview-go/tasks"
+import "fmt"
 
 func main() {
-	tasks.ImportAnagrams()
+	// Инициализация массива чисел
+	arr := [7]int{6, 8, 3, 5, 12, 9, 4}
+
+	// Перебор элементов массива
+	for _, num := range arr {
+		if num > 10 {
+			// Выводим число и выходим из цикла
+			fmt.Println("Число больше 10 найдено:", num)
+			break
+		}
+		fmt.Println("Текущее число:", num)
+	}
 }

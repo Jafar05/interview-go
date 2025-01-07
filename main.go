e38a79908ea1aeca815f3b8d75a80c8938cb54e0
jafar <jafar_magomedov0594@mail.ru>
2025-01-07 11:40:53 +0300

    adding new func

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
+import "interview-go/tasks"
 
 func main() {
-	// Инициализация массива чисел
-	arr := [7]int{6, 8, 3, 5, 12, 9, 4}
-
-	// Перебор элементов массива
-	for _, num := range arr {
-		if num > 10 {
-			// Выводим число и выходим из цикла
-			fmt.Println("Число больше 10 найдено:", num)
-			break
-		}
-		fmt.Println("Текущее число:", num)
-	}
+	tasks.FindProgressCapacity()
 }

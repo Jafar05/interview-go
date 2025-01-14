a2f7a33ce242dd350f956e4a2d710d84cd36fc6f
jafar <jafar_magomedov0594@mail.ru>
2025-01-14 20:34:31 +0300

    update

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
 func main() {
+	// Объявляем переменную мапы, но не инициализируем ее
+	var m map[string]int
 
+	// Попытка записи в неинициализированную (nil) мапу
+	m["key"] = 42 // Здесь произойдет runtime panic: "assignment to entry in nil map"
 }

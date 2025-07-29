2bea4182e4ee582034ce525ea827b79ade28947d
jafar <jafar_magomedov0594@mail.ru>
2025-07-29 16:15:29 +0300

    adding algo tasks

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"sync"
+)
 
+func main() {
+	jobs := make(chan int)
+	res := make(chan int)
+	workerPool(10, square, jobs, res)
+}
+
+func workerPool(id int, f func(int) int, jobs chan int, result chan int) {
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		for val := range jobs {
+			result <- f(val)
+		}
+	}()
+	wg.Wait()
+}
+
+func square(num int) int {
+	return num * num
 }

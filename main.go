71b17227de2c5f774c446e44ddf1ddfe994a4713
jafar <jafar_magomedov0594@mail.ru>
2025-08-03 22:22:40 +0300

    adding algo tasks

--- main.go
+++ main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand/v2"
-	"time"
-)
-
 func main() {
-	chat()
-}
 
-func msg(ch chan string) {
-	message := []string{
-		"Hello",
-		"Hi",
-		"How are you",
-		"Fine",
-	}
-
-	for {
-		ch <- message[rand.IntN(len(message))]
-		time.Sleep(time.Second)
-	}
-	close(ch)
-}
-
-func chat() {
-	ch := make(chan string)
-	go msg(ch)
-	for val := range ch {
-		fmt.Println(val)
-	}
 }

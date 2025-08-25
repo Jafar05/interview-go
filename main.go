242d892cc40f3e06c918e26ed774e7ce3d5cdc8b
jafar <jafar_magomedov0594@mail.ru>
2025-08-25 11:33:19 +0300

    adding algo tasks

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Тестовые случаи
+	testCases := []struct {
+		nums1, nums2 []int
+	}{
+		{[]int{1, 2, 2, 1}, []int{2, 2}}, // [2, 2]
+	}
+
+	for _, test := range testCases {
+		result := intersect(test.nums1, test.nums2)
+		fmt.Printf("nums1: %v, nums2: %v -> %v\n", test.nums1, test.nums2, result)
+	}
+}
+func intersect(nums1 []int, nums2 []int) []int {
+	hash := make(map[int]int)
+	res := []int{}
+	for _, num := range nums1 {
+		hash[num]++
+	}
+	fmt.Println(hash)
+	for _, num := range nums2 {
+		if hash[num] > 0 {
+			res = append(res, num)
+			hash[num]--
+		}
+	}
+	return res
 }
